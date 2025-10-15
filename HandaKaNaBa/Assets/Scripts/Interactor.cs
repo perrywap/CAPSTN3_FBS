039944bf49ebec5c06234008dd43369ff10deb11
Gerald <geraldperry.reyes@benilde.edu.ph>
2025-10-15 21:13:50 +0800

    Added interaction, and task manager

---
+++ HandaKaNaBa/Assets/Scripts/Interactor.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+interface IInteractable
+{
+    public void Interact();
+}
+
+public class Interactor : MonoBehaviour
+{
+    public Transform interactorSource;
+    public float interactRange;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.E))
+        {
+            Ray ray = new Ray(interactorSource.position, interactorSource.forward);
+
+            if(Physics.Raycast(ray, out RaycastHit hitInfo, interactRange))
+            {
+                if(hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObj))
+                {
+                    interactObj.Interact();
+                }
+            }
+        }
+    }
+}
